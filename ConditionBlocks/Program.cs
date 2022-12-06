e9e87c50adabb546f87f81a402fd8a0b17e578af
Laptop <tosun.umutt@gmail.com>
2022-12-06 19:36:34 +0300

    Week 3 Completed.

---
+++ ConditionBlocks/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionBlocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+           int counter = 0;     
+
+           counter++; //counter değerini 1 arttırır.
+           Console.WriteLine(counter);
+
+           counter+=2; //counter değerini 2 arttırır.
+           Console.WriteLine(counter);
+
+           // int operator farkı  ++variable and variable++
+
+           Console.WriteLine(++counter); //önce arttır sonra yazdır.
+           Console.WriteLine(counter++); //önce yazdır sonra arttır.
+           */
+
+
+
+            //Koşul Operatörleri
+
+            /* int number = 6, number2 = 4;
+
+             Console.WriteLine($"{number} {"=="} {number2} sonuç : {number==number2}");
+
+             Console.WriteLine($"{number} {">"} {number2} sonuç : {number>number2}");
+
+             Console.WriteLine($"{number} {"<"} {number2} sonuç : {number<number2}");
+
+             Console.WriteLine($"{number} {"!="} {number2} sonuç : {number!=number2}");
+
+             Console.WriteLine($"{number} {"<="} {number2} sonuç : {number<=number2}");
+
+             Console.WriteLine($"{number} {">="} {number2} sonuç : {number>=number2}");
+
+             */
+            // And -- Or Operator
+
+            /*
+            int firstNumber = 15, secondNumber = 20;
+
+            bool result = (firstNumber < secondNumber) && (secondNumber > 15);
+            Console.WriteLine("And = " + result);
+
+            result = (firstNumber < secondNumber) || (secondNumber > 15);
+            Console.WriteLine("Or = " + result);
+            */
+
+            // if /else-if/else şart blokları -- ister sizden bool expression :)
+            /*
+                        int number = 5;
+                        int secondNumber = 10;
+
+                        // c# ternary operator ? : what is this structure .
+                        string message = number > secondNumber ? "Sayı 1 büyüktür Sayı 2`Den":
+                            number < secondNumber ?
+                            "Sayı 1 küçüktür Sayı 2`Den ":"Sayılar Eşittir";
+
+                        Console.WriteLine(message);
+            */
+
+            Console.Write("Sayı Giriniz : ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Sayı Giriniz : ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            string message = number > secondNumber ? "Sayı 1 büyüktür Sayı 2`Den" :
+                             number < secondNumber ? "Sayı 1 küçüktür Sayı 2`Den " :
+                             "Sayılar Eşittir";
+
+            Console.WriteLine(message);
+
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
--- ConditionBlocks/Properties/AssemblyInfo.cs
+++ ConditionBlocks/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloWorld")]
+[assembly: AssemblyTitle("ConditionBlocks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloWorld")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ConditionBlocks")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("568e6221-a4c8-4740-b3dc-8276f94bb4a9")]
+[assembly: Guid("34cac53c-7bb8-4855-be24-9e8faee28d9e")]
 
 // Version information for an assembly consists of the following four values:
 //
